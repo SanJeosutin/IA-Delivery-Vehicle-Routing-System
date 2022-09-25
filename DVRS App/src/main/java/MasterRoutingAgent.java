8b4431889019314fc89ce2467b1d5bc4e592f72e
Justin San <santosojustin@gmail.com>
2022-09-25 16:27:35 +1000

    Basic Parcel Implemented

--- DVRS App/src/main/java/MasterRoutingAgent.java
+++ DVRS App/src/main/java/MasterRoutingAgent.java
@@ -1,5 +1,15 @@
 import jade.core.Agent;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MasterRoutingAgent extends Agent {
+    private Position position = new Position(0, 0);
+    private List<Parcel> parcels = new ArrayList<Parcel>();
+
+
+    protected void setup() {
+    }
+
 
 }
