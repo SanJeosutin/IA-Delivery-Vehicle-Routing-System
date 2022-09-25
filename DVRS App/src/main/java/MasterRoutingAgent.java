7c4f61642b555ae2c9a250fee4b901363463b97e
Justin San <santosojustin@gmail.com>
2022-09-25 17:41:08 +1000

    Basic Agents communication

--- DVRS App/src/main/java/MasterRoutingAgent.java
+++ DVRS App/src/main/java/MasterRoutingAgent.java
@@ -1,6 +1,10 @@
+import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class MasterRoutingAgent extends Agent {
@@ -9,7 +13,31 @@ public class MasterRoutingAgent extends Agent {
 
 
     protected void setup() {
+        {
+            CyclicBehaviour msgListenBehaviour = new CyclicBehaviour(this) {
+                public void action() {
+                    ACLMessage message = receive();
+                    if (message != null) {
+                        System.out.println(getLocalName() + ": Received response " + message.getContent() + " from " + message.getSender().getLocalName());
+                    }
+                    block();
+                }
+            };
+            addBehaviour(msgListenBehaviour);
+
+            ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+            msg.setContent("Contacting agents...");
+            for (int i = 1; i <= 3; i++) {
+                msg.addReceiver(new AID("d" + i, AID.ISLOCALNAME));
+            }
+
+            System.out.println(getLocalName() + ": Sending message " + msg.getContent() + " to ");
+            Iterator receivers = msg.getAllIntendedReceiver();
+            while (receivers.hasNext()) {
+                System.out.println(((AID) receivers.next()).getLocalName());
+            }
+            send(msg);
+        }
+
     }
-
-
 }
