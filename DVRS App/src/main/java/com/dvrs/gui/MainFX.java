dd5e472b28dafec245902a2935fb4d6fb7ef8426
Justin San <santosojustin@gmail.com>
2022-10-11 11:53:53 +1100

    Updating GUI

--- DVRS App/src/main/java/com/dvrs/gui/MainFX.java
+++ DVRS App/src/main/java/com/dvrs/gui/MainFX.java
@@ -1,9 +1,10 @@
-package com.test.mainfx;
+package com.dvrs.gui;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import org.kordamp.bootstrapfx.BootstrapFX;
@@ -11,21 +12,26 @@ import org.kordamp.bootstrapfx.scene.layout.Panel;
 
 public class MainFX extends Application {
     @Override
-    public void start(Stage primaryStage) throws Exception {                   //(1)
+    public void start(Stage primaryStage) throws Exception {
         Panel panel = new Panel("Delivery Vehicle Routing System");
-        panel.getStyleClass().add("panel-primary");                            //(2)
+
+        panel.getStyleClass().add("panel-primary");
         BorderPane content = new BorderPane();
         content.setPadding(new Insets(20));
+
+        Label headingtxt = new Label("D");
+
         Button button = new Button("Hello BootstrapFX");
-        button.getStyleClass().setAll("btn","btn-danger");                     //(2)
+        button.getStyleClass().setAll("btn", "btn-danger");
+
         content.setCenter(button);
         panel.setBody(content);
 
-        button.setOnAction( actionEvent ->
+        button.setOnAction(actionEvent ->
                 System.out.println("Hello World"));
 
         Scene scene = new Scene(panel);
-        scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());       //(3)
+        scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
 
         primaryStage.setTitle("DVRS");
         primaryStage.setScene(scene);
